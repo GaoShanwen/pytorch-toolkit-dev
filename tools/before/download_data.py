c63431952e7440b4101fd8203bb659d95f8aedb4
gaowenjie88 <gaowenjie@rongxwy.com>
2024-08-15 01:20:06 +0800

    :zap: optimize download data

--- tools/before/download_data.py
+++ tools/before/download_data.py
@@ -1,10 +1,9 @@
-import os
-import multiprocessing
-
-# import wget
+import aiohttp
+import asyncio
 import argparse
-import requests
+import os
 import tqdm
+
 
 def parse_args():
     parser = argparse.ArgumentParser(description='Download data from urls file')
@@ -29,32 +28,41 @@ def load_csv_file(label_file: str, idx_column: int=1, url_column: int=3):
     return product_id_map, len(lines)-1
 
 
-def copy_file(file_info):
-    ori_path, dst_path = file_info
-    try:
-        # wget.download(ori_path, dst_path, bar=False)
-        base_name = os.path.basename(ori_path)
-        response = requests.get(ori_path).content
-        with open(os.path.join(dst_path, base_name), 'wb') as file:
-            file.write(response)
-    except:
-        # print(f"{ori_path, dst_path} is error!")
-        pass
- 
 
-def main(download_urls, dst_root):
-    res, copyed_num = load_csv_file(download_urls, idx_column=1, url_column=2)
-    result_list_tqdm = []
-    file_infos = []
-    for i, (cat, urls) in enumerate(res.items()):
+async def download_file(url, save_path):
+    try:
+        async with semaphore:
+            async with aiohttp.ClientSession() as session:
+                async with session.get(url) as response:
+                    with open(save_path, 'wb') as f:
+                        while True:
+                            chunk = await response.content.read(1024)
+                            if not chunk:
+                                break
+                            f.write(chunk)
+            pbar.update()
+    except Exception as e:
+        raise Exception(f"Error downloading {url}: {e}")
+
+
+async def main(download_urls, dst_root, workers=50):
+    res, _ = load_csv_file(download_urls, idx_column=1, url_column=2)
+    tasks = []
+
+    global semaphore
+    semaphore = asyncio.Semaphore(workers)
+    for cat, urls in res.items():
         obj_dir = os.path.join(dst_root, cat)
         if not os.path.exists(obj_dir):
             os.makedirs(obj_dir)
         for url in urls:
-            file_infos.append([url, obj_dir])
+            obj_file = os.path.join(obj_dir, os.path.basename(url))
+            tasks.append(download_file(url, obj_file))
     
-    with multiprocessing.Pool(processes=12) as pool:
-        list(tqdm.tqdm(pool.imap(copy_file, file_infos), total=copyed_num))
+    global pbar
+    pbar = tqdm.tqdm(total=len(tasks), desc=f"download img to {tag}_{brand_id}/{task}")
+    await asyncio.gather(*tasks, return_exceptions=False)
+    pbar.close()
 
 
 if __name__ == "__main__":
@@ -69,5 +77,4 @@ if __name__ == "__main__":
     if not os.path.exists(obj_root):
         os.makedirs(obj_root)
 
-    main(urls_file, obj_root)
-
+    asyncio.run(main(urls_file, obj_root))
