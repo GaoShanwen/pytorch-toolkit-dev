7525391bf33d5eeed3a5cdad9e9d21100008c104
gaowenjie <875710462@qq.com>
2026-08-11 17:30:21 +0800

    modify convert code

--- tools/convert/convert_onnx2trt.py
+++ tools/convert/convert_onnx2trt.py
@@ -1,5 +1,6 @@
 import tensorrt as trt
 import os
+import sys
 
 def build_engine(onnx_file_path, engine_file_path, fp16_mode=True, max_workspace_size=4):
     """
@@ -68,12 +69,14 @@ def build_engine(onnx_file_path, engine_file_path, fp16_mode=True, max_workspace
 
 
 # 使用示例
-if __name__ == "__main__": 
-    onnx_path = "models/onnx/yolo26s-pose-c15k7v260728p.onnx"
+if __name__ == "__main__":
+    suffix = sys.argv[1] if len(sys.argv) > 1 else ''
+    assert suffix != '' and len(suffix) == 4, "suffix must be MMDD, e.g. 0811"
+    onnx_path = f"models/onnx/yolo26s-pose-c15k7v26{suffix}.onnx"
     engine_path = onnx_path.replace("onnx", "engine")
 
     build_engine(
         onnx_file_path=onnx_path,
         engine_file_path=engine_path,
-        fp16_mode=True      # 不使用 FP16 加速
+        fp16_mode=True
     )
